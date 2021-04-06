4e7a2dd91ee23902293426af8b0680ef48ac4d60
andrewxcon <39475057+andrewxcon@users.noreply.github.com>
2021-04-05 21:46:57 -0500

    Update m1.c

--- m1.c
+++ m1.c
@@ -38,8 +38,6 @@ int main( int argc, char *argv[] ) {
     int block_total = bit_value / bit_block_value;
     block_total = block_total * 8;
 
-    printf("Total # Blocks:               %d\n", block_total);
-
     //INDEX 
     double bit_block = log( bSize) / log( 2 );
     double bit_asso = log( asso ) / log( 2 );
@@ -47,28 +45,32 @@ int main( int argc, char *argv[] ) {
 
     int index  = ( int) bit_size - ( (int) bit_block + (int) bit_asso);
 
-    printf("Index Size:                   %d bits\n", index);
 
     //TAG SIZE
     int tag = 32 -  (int) bit_block - index;
-    printf("Tag Size:                     %d bits\n", tag);
+
 
     //NUMBER OF ROWS
     int rows = block_total / asso;
-    printf("Total # Rows:                 %d\n", rows);
 
     //OVERHEAD
     int overhead = ((tag + 1) * block_total) / 8;
-    printf("Overhead Size:                %d bytes\n", overhead);
 
     //IMPLEMATION SIZE
     int overhead_convert = overhead / 1024;
     int act_size = cSize + overhead_convert;
-    printf("Implementation Memory Size:   %d\n", act_size);
 
     //COST
     double cost = act_size * 0.07;
+
+    printf("Total # Blocks:               %d\n", block_total);
+    printf("Tag Size:                     %d bits\n", tag);
+    printf("Index Size:                   %d bits\n", index);
+    printf("Total # Rows:                 %d\n", rows);
+    printf("Overhead Size:                %d bytes\n", overhead);
+    printf("Implementation Memory Size:   %d\n", act_size);
     printf("Cost:                         %.2lf\n", cost);
+
 
     printf("\n");
 
