da2f2bc882d26fb2d5951c0217eba2f3dbf637c5
andrewxcon <39475057+andrewxcon@users.noreply.github.com>
2021-04-25 10:02:32 -0500

    Update m1.c

--- m1.c
+++ m1.c
@@ -4,8 +4,13 @@
 #include <math.h> 
 
 #include "ParseInput.h"
+#include "ParseHex.h"
 
-signed int binaryToHex(char* value) {
+int fromBinary(const char *s) {
+    return (int) strtol(s, NULL, 2);
+ }
+
+ signed int binaryToHex(char* value) {
     char *a = value;
     int num = 0;
     do {
@@ -15,144 +20,6 @@ signed int binaryToHex(char* value) {
     } while (*a);
 
     return num;
-}
-
-void HexToBin(char* hexdec, int tag, int index, char* instLength) {
- 
-    long int i = 0;
-    char binaryNum[33] = "";
-
-    if( strlen(hexdec) != 8) {
-        strcat(binaryNum, "0000");
-    }
-
-    while (hexdec[i]) {
- 
-        switch (hexdec[i]) {
-        case '0':
-            // printf("0000");
-            strcat(binaryNum, "0000");
-            break;
-        case '1':
-            // printf("0001");
-            strcat(binaryNum, "0001");
-            break;
-        case '2':
-            // printf("0010");
-            strcat(binaryNum, "0010");
-            break;
-        case '3':
-            // printf("0011");
-            strcat(binaryNum, "0011");
-            break;
-        case '4':
-            // printf("0100");
-            strcat(binaryNum, "0100");
-            break;
-        case '5':
-            // printf("0101");
-            strcat(binaryNum, "0101");
-            break;
-        case '6':
-            // printf("0110");
-            strcat(binaryNum, "0110");
-            break;
-        case '7':
-            // printf("0111");
-            strcat(binaryNum, "0111");
-            break;
-        case '8':
-            // printf("1000");
-            strcat(binaryNum, "1000");
-            break;
-        case '9':
-            // printf("1001");
-            strcat(binaryNum, "1001");
-            break;
-        case 'A':
-        case 'a':
-            // printf("1010");
-            strcat(binaryNum, "1010");
-            break;
-        case 'B':
-        case 'b':
-            // printf("1011");
-            strcat(binaryNum, "1011");
-            break;
-        case 'C':
-        case 'c':
-            // printf("1100");
-            strcat(binaryNum, "1100");
-            break;
-        case 'D':
-        case 'd':
-            // printf("1101");
-            strcat(binaryNum, "1101");
-            break;
-        case 'E':
-        case 'e':
-            // printf("1110");
-            strcat(binaryNum, "1110");
-            break;
-        case 'F':
-        case 'f':
-            // printf("1111");
-            strcat(binaryNum, "1111");
-            break;
-        default:
-            printf("\nInvalid hexadecimal digit");
-        }
-        i++;
-    }
-
-    int j = 0, k = 0, z = 28, l = 0;
-
-    char tagValue[tag];
-    char indexValue[tag];
-    char offsetValue[5];
-
-    int indexS = 28 - index;
-    int tagS = 28 - index - tag;
-
-    while (z <= 32) {
-        offsetValue[l] = binaryNum[z];
-        z++;
-        l++;
-    }
-
-    while(indexS < 28) {
-        indexValue[k] = binaryNum[indexS];
-        k++;
-        indexS++;
-    }
-
-    while(tagS < 28 - index) {
-        tagValue[j] = binaryNum[tagS];
-        j++;
-        tagS++;
-    }
-
-    tagValue[j++] = '\0';
-    indexValue[k++] = '\0';
-    offsetValue[z++] = '\0';
-
-    signed int offsetHEX = binaryToHex(offsetValue);
-    signed int indexHEX = binaryToHex(indexValue);
-    signed int tagHEX = binaryToHex(tagValue);
-
-    printf("HEX INSTR: 0x%s\n", hexdec);
-    printf("BINARY: %s\n", binaryNum);
-    printf("OFF: %X\n", offsetHEX);
-    printf("IND: %X\n", indexHEX);
-    printf("TAG: %X\n", tagHEX);
-    printf("INSTRLENGTH: %s\n", instLength);
-    printf("\n");
-
-    // printf("instLength %d\n", atoi(instLength));
-    // printf("offsetValue %s\n", offsetValue);
-    // printf("tagValue %s\n", tagValue);
-    // printf("indexValue %s\n", indexValue);
-    // printf("binaryNum %s\n", binaryNum);
 }
 
 void m1(ParsedArgs *myIns) {
@@ -217,12 +84,12 @@ void m1(ParsedArgs *myIns) {
     //IMPLEMATION SIZE
     int overhead_convert = overhead / 1024;
     int act_size = cSize + overhead_convert;
-    int act_size_bytes = act_size * 1024;
+	int act_size_bytes = act_size * 1024;
 
     //COST
     double cost = act_size * 0.09; // 9 cents per KB as per instruction file
 	
-	  printf("***** Cache Calculated Values *****\n\n");
+	printf("***** Cache Calculated Values *****\n\n");
 
     printf("Total # Blocks:               %d\n", block_total);
     printf("Tag Size:                     %d bits\n", tag);
@@ -236,6 +103,18 @@ void m1(ParsedArgs *myIns) {
     int count = 0;
     int line_count = 1;
     char real_size[3];
+    struct cache newCache[rows][asso];
+    int i, j;
+    int missCount;
+    int hitCount;
+    int readCount;
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < asso; j++) {
+            newCache[i][j].tag = (char *)malloc(sizeof(char)*(tag+1));
+            newCache[i][j].valid = 0;
+        } 
+    }
 
     // Parse trace file
     while (fgets(line, sizeof(line), file)) {
@@ -243,37 +122,128 @@ void m1(ParsedArgs *myIns) {
         signed int address_two;
         signed int address_three;
         char reg[20], temp[20], str[20];
-        char size[5];
-
+        char size[5];  
+        char binaryNum[100];
+        char a[3][100];
+        int addressArr[3];
+        int cnt;
+        
+        //fine addresses in file
         if(line_count == 1) {
             sscanf(line, "%s %s %x", reg, size, &address);
             real_size[0] = size[1];
             real_size[1] = size[2];
             // printf("0x%x: 00%s\n", address, real_size); 
             sprintf(str, "%x", address);
-            HexToBin(str, tag, index, real_size);
+            strcpy(binaryNum, "");
+            HexToBin(str, binaryNum);
+            strcpy(a[0], binaryNum);
+            addressArr[0] = address;
+            count++;
          } else if(line_count == 2) {
+            addressArr[0] = -1;
             sscanf(line, "%s %x %s %s %x %s", temp, &address_two, temp, temp, &address_three, temp);
             if(address_two > 0) {
-                // printf("0x%x: 00%s\n", address_two, real_size);
                 count++;
+                strcpy(binaryNum, "");
+                HexToBin(str, binaryNum);
+                strcpy(a[1], binaryNum);
+                addressArr[1] = address_two;
+            } else {
+                strcpy(a[1], "");
+                addressArr[1] = -1;
             }
             if(address_three > 0) {
-                // printf("0x%x: 00%s\n", address_three, real_size);
                 count++;
+                strcpy(binaryNum, "");
+                HexToBin(str, binaryNum);
+                strcpy(a[2], binaryNum);
+                addressArr[2] = address_three;
+            } else {
+                strcpy(a[2], "");
+                addressArr[2] = -1;
             }
         } else {
             line_count = 0;
         }
 
-        line_count++;
+        //loop through address array and add to cache
+        for(cnt = 0; cnt < line_count; ++cnt) {
+            if(strcmp(a[cnt], "") != 0 && addressArr[cnt] != -1) {
+                int j = 0, k = 0, z = 28, l = 0;
+                int oSize = log( bSize ) / log( 2 );
+                char tagValue[tag];
+                char indexValue[index];
+                char offsetValue[oSize];
+                int indexS = 32 - oSize - index;
+                int tagS = 32 - oSize - index - tag;
+                int iIndex;
 
-        //TODO: TAKE THIS OUT
+                while (z <= 32) {
+                    offsetValue[l] = a[cnt][z];
+                    z++;
+                    l++;
+                }
+    
+                while(indexS < 32 - oSize) {
+                    indexValue[k] = a[cnt][indexS];
+                    k++;
+                    indexS++;
+                }
+    
+                while(tagS < 32 - oSize - index) {
+                    tagValue[j] = a[cnt][tagS];
+                    // printf("%d", tagValue[j]);
+                    j++;
+                    tagS++;
+                }
+    
+                iIndex = fromBinary(indexValue);
+    
+                for(i = 0; i < asso; i++) {
+                    if(newCache[iIndex][i].valid == 0) {
+                        newCache[iIndex][i].valid = 1;
+                        newCache[iIndex][i].tag = tagValue;
+                        missCount += 1;
+                        break;
+                    } else if(newCache[iIndex][i].valid == 1) {
+                        // if(strcmp(newCache[iIndex][i].tag, tagValue) == 0) {
+                        //     hitCount += 1 
+                        // }
+                    } else {
+                        // random replacement
+                        int num = (rand() % (asso - 0 + 1)) + 0;
+                        newCache[iIndex][num].tag = tagValue;
+                    }
+                }
+
+
+                printf("INST: %x\n", atoi(real_size));
+
+                // tagValue[j++] = '\0';
+                indexValue[k++] = '\0';
+                offsetValue[z++] = '\0';
+                // printf("BLOCK: %d\n", bSize);
+                // printf("SEE: %x\n", addressArr[cnt]);
+                // printf("SEE2: %x\n", addressArr[cnt] + 4);
+                // signed int off = binaryToHex(indexValue);
+                // printf("%s\n", indexValue);
+                // printf("%x\n", off);
+    
+            }
+        }
+
         if(count == 20) {
             break;
         }
+
+        line_count++;
     }
+
+    printf("MISS: %d\n", missCount);
+
 
     fclose(file);
 }
+
 
